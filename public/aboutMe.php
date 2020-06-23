069706cd5478694315417aa675ad8c13463a7919
ruddnr88 <ruddnr88@naver.com>
2020-06-23 12:56:28 +0900

    dd

---
+++ public/aboutMe.php
@@ -0,0 +1,7 @@
+<?php
+include "../part/head.php";
+?>
+<h1 class="con">About Me</h1>
+<?php
+include "../part/foot.php";
+?>
---
+++ public/index.php
@@ -0,0 +1,75 @@
+<?php
+include "../part/head.php";
+?>
+<div class="con grid image-grid ">
+    <div class="grid-block">
+        <div class="tile ">
+            <a class="tile-link" href="#">
+                <img
+                    class="tile-img tile-img1"
+                    src="https://lh3.googleusercontent.com/pw/ACtC-3eqDpYXSzQK9Gr8Dm6pRBTXg65teqvPUlndvrG31BEmbHGYSiGja4ZhpI86_b5pYG_nWHZvi0-a2svpmvqtfGHSqLAypliNdl9vI-xGKT0XixvVSzroZ0e7HXFeyVoNyU5XMuMoEzf5f6VgQbmIO2yr=w1384-h791-no"
+                    alt="Image">
+            </a>
+        </div>
+    </div>
+
+    <div class="grid-block">
+        <div class="tile">
+            <a class="tile-link" href="#">
+                <img
+                    class="tile-img tile-img2"
+                    src="https://lh3.googleusercontent.com/pw/ACtC-3eO9L51TGiTghLao-VLNhO_C0egdgv7NfamlpdYbMAKCfXNlkk7WPPcxMJTaU9hO-HNnTqUivtavZ-6iK9mzoq0Qf3kJ5MAcnCoDUqbzd8VzpFKhu3mqDYZBG0KNGVxNHSEUwUiTxCUEFf_yFnNNLL0=w1384-h791-no"
+                    alt="Image">
+            </a>
+        </div>
+    </div>
+
+    <div class="grid-block">
+        <div class="tile">
+            <a class="tile-link" href="#">
+                <img
+                    class="tile-img tile-img3"
+                    src="https://lh3.googleusercontent.com/pw/ACtC-3dhqk2CMZiOyFNPfjfffFIRIp1r9XjSNhfWHXk3gHDNf0hGwwWtXlQbtTf7DBp9t_KnCVull_WGpXNDVRtLkslw1qskUAjbSMEilFituKSeJVKqzVbsTFOeLfvd92nowevb7EGG3Pno37_WfsShZnYP=w1384-h791-no"
+                    alt="Image">
+            </a>
+        </div>
+    </div>
+
+    <div class="grid-block">
+        <div class="tile">
+            <a class="tile-link" href="#">
+                <img
+                    class="tile-img tile-img6"
+                    src="https://lh3.googleusercontent.com/pw/ACtC-3csXE_23DqssKWd76nUGNa5re7em4ySmZEif2L_jxJBpIV0pV3qHYXQope682nX2Qs04nhMHVZlNNwbUGzz6CWjaywX5VaH5TX2Wrh0iocAk5aRrN2ud7H55mGYdR-z-QEyK5ckiZ4BGZLiSpXe-TmD=w1384-h791-no"
+                    alt="Image">
+            </a>
+        </div>
+    </div>
+
+    <div class="grid-block">
+        <div class="tile">
+            <a class="tile-link" href="#">
+                <img
+                    class="tile-img tile-img7"
+                    src="https://lh3.googleusercontent.com/pw/ACtC-3fbpkwsUjaB8Cv6JFW_Z5E_7WjTwR3hEfafXoDmSsxBN_I_TKHLw3ngMCcOhYVv0KrzdJpjBfRzmTPVUikRZpyy53lb10ENbvWxrP-Hf83Y1KXc2RG0zLXoXqQjaT7NP9bBKEY7iv2I8sRIUJxCU9ql=w1703-h973-no"
+                    alt="Image">
+            </a>
+        </div>
+    </div>
+
+    <div class="grid-block">
+        <div class="tile">
+            <a class="tile-link" href="#">
+                <img
+                    class="tile-img tile-img8"
+                    src="https://lh3.googleusercontent.com/pw/ACtC-3cCwp7dKtvwMA8uf_Y3OTxZz-CiYRpay0fSZhfqYXCeDWBBfQERYnB7DzDCbsyyzdzoOU7_9BbW3WR5VaY9YPD3syn3VgZDxj6-2qTOwDw_HDlnLd92LlvCZIEr8oDKVt4AU7q-oJNHE6O9sRhGAVMi=w1384-h791-no"
+                    alt="Image">
+            </a>
+        </div>
+    </div>
+
+</div>
+
+<?php
+include "../part/foot.php";
+?>
